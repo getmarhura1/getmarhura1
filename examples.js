25a279b490aac067d43cf4577fa500ddef8a413d
getmarhura1 <getmarhura1@Admins-MacBook-Air.local>
2023-04-13 23:09:49 +0800

    first commit

---
+++ examples.js
@@ -0,0 +1,72 @@
+---
+layout:   page
+title:    noBackend Example Apps
+tab:      examples
+---
+
+<h2>noBackend by example</h2>
+
+<article>
+  <h3>
+    Invoice App
+    <small>(based on <a href="http://css-tricks.com/editable-invoice-v2/">EditableInvoice by Chris Coyer</a>)</small>
+  </h3>
+  <p>
+    <a href="http://invoice.nobackend.org/" class="screenshot" title="noBackend invoice">
+      <img alt="" src="{{ page.root_path }}assets/example-apps/invoicing-app-screenshot.png">
+    </a>
+  </p>
+  <p>
+    Inspired by the wonderful <a href="http://todomvc.com/">TodoMVC</a>,
+    we created an example invoicing app that is implemented
+    using today's noBackend solutions.
+  </p>
+
+  <p>
+    It's based on Chris Coyier's <a href="http://css-tricks.com/editable-invoice-v2/">Editable Invoice</a>,
+    that he created back in 2009 as a showcase for an HTML-only app.
+    We've tweaked the design a bit, and also added features that
+    typically require a backend: User Accounts, Exports, Emails.
+  </p>
+  <p>
+    <a href="http://invoice.nobackend.org/" class="btn">open Invoice App</a>
+    | <a href="https://github.com/noBackend/noBackend-app-invoice">source code</a>
+  </p>
+</article>
+
+<article>
+  <h3>
+    Troopr
+  </h3>
+  <p>
+    <a href="https://brilliant-fire-3777.firebaseapp.com" class="screenshot" title="noBackend Troopr">
+      <img alt="" src="{{ page.root_path }}assets/example-apps/troopr.png">
+    </a>
+  </p>
+  <p>
+    Wanting to create a simple Front-End Driven CRUD application with one of the various noBackend solutions was easy enough with <a href="http://firebase.com/">Firebase</a> & <a href="http://hood.ie">Hoodie</a>.
+  </p>
+
+  <p>
+    After building the out Troopr with Firebase and then with Hoodie over the course of a weekend, I looked into the new hosting service provided by Firebase to deploy Troopr for others to see without forking the GitHub repo and setting it up themselves. The entire deployment process took me less than a minute, and then the app was live!
+  </p>
+  <p>
+    I am currently looking into various options for hosting the Hoodie-backed version of Troopr, having narrowed it down to Nodejitsu and AppBack. The source for that project is included in the same repo as the Firebase-backed Troopr.
+  </p>
+  <p>
+    All pull requests are welcome, I would like to keep improving Troopr over time as I learn better ways to build out these Front-End Driven Apps.
+  </p>
+  <p>
+    <a href="https://brilliant-fire-3777.firebaseapp.com" class="btn">open Troopr</a>
+    | <a href="https://github.com/hipsterbrown/FDA-demos">source code</a>
+  </p>
+</article>
+
+<div class="box">
+  <h3>
+    Want to add another example app?
+  </h3>
+  <p>
+     <a href="https://twitter.com/nobackend">Get in touch</a>!
+  </p>
+</div>
---
+++ help_us.php
@@ -0,0 +1,16 @@
+---
+layout:   page
+title:    Welcome to noBackend!
+---
+
+<h2>We could use another hand!</h2>
+
+<p>
+  This is early days of noBackend. Help us, by sending over
+  feedback, spreading the word, <a href="{{ site.github_url }}">fixing some typos</a>,
+  make a shiny design for the website, <a href="https://github.com/nobackend/nobackend-app-invoice/">add an implementation of another
+  noBackend solution for our example app</a>, add entirely new
+  example apps, add new noBackend solutions. What ever comes
+  to your mind, this is a community endeavor, and you are more
+  than invited to help making it prosper.
+</p>
